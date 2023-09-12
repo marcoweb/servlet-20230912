39b3588837e4cab102a0b1fb3ee9c5226af25cee
marcoweb <marcocarvalho.web@gmail.com>
2023-09-12 17:40:55 +0000

    Primeiro Servlet

---
+++ app/src/main/java/exemploservlet/IndexServlet.java
@@ -0,0 +1,19 @@
+package exemploservlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "IndexServlet", urlPatterns = "/")
+public class IndexServlet extends HttpServlet {
+    public void doGet(
+        HttpServletRequest request,
+        HttpServletResponse response) 
+        throws ServletException, IOException {
+            response.getWriter().print("Olá Servlet");
+    }
+}
